894cb4db2abe6fff43388e3001c20155006995d3
Jonathan Peña <jonathanpena@Penas-Macbook.lan>
2023-05-10 19:00:27 -0700

    fixed a bug but now we have to fix each reoccurence

--- menu.py
+++ menu.py
@@ -15,6 +15,10 @@ class TimePicker(tk.Frame):
         self.master = master
         self.months = ['January', 'February', 'March', 'April', 'May', 'June',
                        'July', 'August', 'September', 'October', 'November', 'December']
+        # Create a dictionary to map month names to their corresponding numbers
+        self.months = {'January': 1,'February': 2,'March': 3,'April': 4,'May': 5,'June': 6,
+                           'July': 7,'August': 8,'September': 9, 'October': 10, 'November': 11,'December': 12
+                          }
         self.days = list(range(1, 32))
         self.years = list(range(2023, 2101))
         self.hours = list(range(1, 13))
@@ -23,7 +27,6 @@ class TimePicker(tk.Frame):
         self.am_pm2 = ['PM']
         self.task_name = tk.StringVar()
         self.event_type_var = tk.StringVar(self)
-        self.reoccurring_freq_var = tk.StringVar(self, value='')
         self.event_type_var.set('Single')
         self.event_types = ['Single', 'Reoccurring', 'Anti-Task']
         self.reoccurring_options = ['Daily', 'Weekly','BiWeekly', 'Monthly', 'Yearly']
@@ -35,7 +38,7 @@ class TimePicker(tk.Frame):
         self.month_label = tk.Label(self, text='Month:')
         self.month_label.grid(row=0, column=0)
         self.month_var = tk.StringVar(self)
-        self.reoccurring_freq_var = tk.StringVar(self)
+        self.reoccurring_freq_var = tk.StringVar(self, value='')
         self.month_var.set(self.months[0])
         self.month_dropdown = ttk.Combobox(self, textvariable=self.month_var, values=self.months)
         self.month_dropdown.grid(row=0, column=1)
@@ -99,7 +102,7 @@ class TimePicker(tk.Frame):
         self.end_am_pm_checkbox_pm = tk.Checkbutton(self, text='PM', variable=self.end_am_pm_var, onvalue='PM', offvalue='')
         self.end_am_pm_checkbox_pm.grid(row=5, column=4)
 
-############################################I think the bug is around here #############################################
+
         # Create the dropdown menu for event type
         self.event_type_label = tk.Label(self, text='Event Type:')
         self.event_type_label.grid(row=7, column=0)
@@ -123,22 +126,25 @@ class TimePicker(tk.Frame):
         elif event_type == 'Reoccurring':
             # Show the reoccurring options
             self.show_reoccurring_options()
-            self.reoccurring_freq_var = tk.StringVar(self)
+        elif event_type == 'Anti-Task':
+            # Hide the reoccurring options
+            self.hide_reoccurring_options()
 
     def hide_reoccurring_options(self):
         # Remove the reoccurring option dropdown if it exists
         if hasattr(self, 'reoccurring_option_dropdown'):
-                    self.reoccurring_option_dropdown.destroy()
+            self.reoccurring_option_dropdown.destroy()
 
     def show_reoccurring_options(self):
+
         # Create the dropdown menu for reoccurring options
         self.hide_reoccurring_options()
-        self.reoccurring_option_var = tk.StringVar(self)
-        self.reoccurring_option_var.set(self.reoccurring_options[0])
-        self.reoccurring_option_dropdown = ttk.OptionMenu(self, self.reoccurring_option_var, self.reoccurring_options[0], *self.reoccurring_options)
+        self.reoccurring_freq_var = tk.StringVar(self)
+        self.reoccurring_freq_var.set('') # Set the default value here
+        self.reoccurring_option_dropdown = ttk.OptionMenu(self, self.reoccurring_freq_var, *self.reoccurring_options)
         self.reoccurring_option_dropdown.grid(row=7, column=2)
 
-################################################^^^^^^^#############################################
+
     def submit(self):
         month = self.month_var.get()
         day = self.day_var.get()
@@ -152,8 +158,8 @@ class TimePicker(tk.Frame):
         event_type = self.event_type_var.get()
         task_name = self.task_name_entry.get()
         self.task_name = task_name
-        ###########Not sure if this goes here############
-        freq = self.reoccurring_freq_var.get()
+    
+        freq = self.reoccurring_freq_var.get() # Get the frequency here
 
 
         # Format time string
@@ -162,8 +168,8 @@ class TimePicker(tk.Frame):
         end_minute = f"{int(end_minute):02d}"
         end_am_pm = self.end_am_pm_var.get()
         
-        time_str = f'{month} {day}, {year} - {start_hour}:{start_minute}{start_am_pm} to {end_hour}:{end_minute}{end_am_pm} ({event_type}:{task_name})'
-
+        time_output = f'{month} {day}, {year} - {start_hour}:{start_minute}{start_am_pm} to {end_hour}:{end_minute}{end_am_pm} ({event_type}:{task_name})'
+        time_str = f'{month} {day}, {year} - {start_hour}:{start_minute}{start_am_pm} to {end_hour}:{end_minute}{end_am_pm}'
          # Validate input
         try:
                 # Convert start and end times to 24-hour format for comparison
@@ -196,19 +202,17 @@ class TimePicker(tk.Frame):
                 # Write the latest input to the file
                 with open('user_input.txt', 'a') as f:
                     f.write(f"{time_str}\n")
-                    self.msg_display.config(text=f"Your appointment is booked for {time_str}.")
+                    self.msg_display.config(text=f"Your appointment is booked for {time_output}.")
         
         elif event_type == 'Reoccurring':
-            ##############This part is not working###############
             print("Reoccurring")
             print(freq)
             print("its not working")
 
             if freq == 'Daily':
                 print("Reoccurring Daily")
-                ########This does not print so it must mean that "freq is not being called"
                 # Get the user's selected start date
-                start_date = datetime.date(year, month, day)
+                start_date = datetime.date(int(year), int(month), int(day))
 
                 # Iterate through the rest of the year, incrementing the date by one day
                 current_date = start_date
@@ -217,7 +221,8 @@ class TimePicker(tk.Frame):
                     current_date_str = current_date.strftime('%B %d, %Y')
                     
                     # Create a task for the current date
-                    time_str = f'{current_date_str} - {start_hour}:{start_minute}{start_am_pm} to {end_hour}:{end_minute}{end_am_pm} ({event_type}:{task_name})'
+                    time_output = f'{month} {day}, {year} - {start_hour}:{start_minute}{start_am_pm} to {end_hour}:{end_minute}{end_am_pm} ({event_type}:{task_name})'
+                    time_str = f'{current_date_str} - {start_hour}:{start_minute}{start_am_pm} to {end_hour}:{end_minute}{end_am_pm}'
                     
                     # Write the task to the file
                     with open('user_input.txt', 'a') as f:
@@ -226,11 +231,11 @@ class TimePicker(tk.Frame):
                     # Increment the date by one day
                     current_date += datetime.timedelta(days=1)
                 
-                self.msg_display.config(text=f"Your appointment is booked for {time_str}:{freq}")
+                self.msg_display.config(text=f"Your appointment is booked for {time_output}:{freq}")
 
             elif freq == 'Weekly':
                 # Get the user's selected start date
-                start_date = datetime.date(year, month, day)
+                start_date = datetime.date(int(year), int(month), int(day))
 
                 # Iterate through the rest of the year, incrementing the date by one day
                 current_date = start_date
@@ -238,9 +243,10 @@ class TimePicker(tk.Frame):
                     # Format the current date as a string for display
                     current_date_str = current_date.strftime('%B %d, %Y')
                     
-                    # Create a task for the current date
-                    time_str = f'{current_date_str} - {start_hour}:{start_minute}{start_am_pm} to {end_hour}:{end_minute}{end_am_pm} ({event_type}:{task_name})'
-                    
+                   # Create a task for the current date
+                    time_output = f'{month} {day}, {year} - {start_hour}:{start_minute}{start_am_pm} to {end_hour}:{end_minute}{end_am_pm} ({event_type}:{task_name})'
+                    time_str = f'{current_date_str} - {start_hour}:{start_minute}{start_am_pm} to {end_hour}:{end_minute}{end_am_pm}'
+
                     # Write the task to the file
                     with open('user_input.txt', 'a') as f:
                         f.write(f"{time_str}\n")
@@ -248,11 +254,11 @@ class TimePicker(tk.Frame):
                     # Increment the date by one day
                     current_date += datetime.timedelta(days=7)
                 
-                self.msg_display.config(text=f"Your appointment is booked for {time_str}:{freq}")
+                self.msg_display.config(text=f"Your appointment is booked for {time_output}:{freq}")
 
             elif freq == 'BiWeekly':
                 # Get the user's selected start date
-                start_date = datetime.date(year, month, day)
+                start_date = datetime.date(int(year), int(month), int(day))
 
                 # Iterate through the rest of the year, incrementing the date by one day
                 current_date = start_date
@@ -260,9 +266,10 @@ class TimePicker(tk.Frame):
                     # Format the current date as a string for display
                     current_date_str = current_date.strftime('%B %d, %Y')
                     
-                    # Create a task for the current date
-                    time_str = f'{current_date_str} - {start_hour}:{start_minute}{start_am_pm} to {end_hour}:{end_minute}{end_am_pm} ({event_type}:{task_name})'
-                    
+                   # Create a task for the current date
+                    time_output = f'{month} {day}, {year} - {start_hour}:{start_minute}{start_am_pm} to {end_hour}:{end_minute}{end_am_pm} ({event_type}:{task_name})'
+                    time_str = f'{current_date_str} - {start_hour}:{start_minute}{start_am_pm} to {end_hour}:{end_minute}{end_am_pm}'
+
                     # Write the task to the file
                     with open('user_input.txt', 'a') as f:
                         f.write(f"{time_str}\n")
@@ -270,11 +277,11 @@ class TimePicker(tk.Frame):
                     # Increment the date by one day
                     current_date += datetime.timedelta(days=14)
                 
-                self.msg_display.config(text=f"Your appointment is booked for {time_str}:{freq}")
+                self.msg_display.config(text=f"Your appointment is booked for {time_output}:{freq}")
             
             elif freq == 'Monthly':
                 # Get the user's selected start date
-                start_date = datetime.date(year, month, day)
+                start_date = datetime.date(int(year), int(month), int(day))
 
                 # Iterate through the rest of the year, incrementing the date by one month
                 current_date = start_date
@@ -282,8 +289,9 @@ class TimePicker(tk.Frame):
                     # Format the current date as a string for display
                     current_date_str = current_date.strftime('%B %d, %Y')
 
-                    # Create a task for the current date
-                    time_str = f'{current_date_str} - {start_hour}:{start_minute}{start_am_pm} to {end_hour}:{end_minute}{end_am_pm} ({event_type}:{task_name})'
+                   # Create a task for the current date
+                    time_output = f'{month} {day}, {year} - {start_hour}:{start_minute}{start_am_pm} to {end_hour}:{end_minute}{end_am_pm} ({event_type}:{task_name})'
+                    time_str = f'{current_date_str} - {start_hour}:{start_minute}{start_am_pm} to {end_hour}:{end_minute}{end_am_pm}'
 
                     # Write the task to the file
                     with open('user_input.txt', 'a') as f:
@@ -292,11 +300,11 @@ class TimePicker(tk.Frame):
                     # Increment the date by one month
                     current_date += relativedelta(months=1)
                 
-                self.msg_display.config(text=f"Your appointment is booked for {time_str}:{freq}")
+                self.msg_display.config(text=f"Your appointment is booked for {time_output}:{freq}")
             
             elif freq == 'Yearly':
                 # Get the user's selected start date
-                start_date = datetime.date(year, month, day)
+                start_date = datetime.date(int(year), int(month), int(day))
 
                 # Set a maximum number of years to schedule the task
                 max_years = 5
@@ -308,8 +316,9 @@ class TimePicker(tk.Frame):
                     current_date_str = start_date.replace(year=current_year).strftime('%B %d, %Y')
                     
                     # Create a task for the current date
-                    time_str = f'{current_date_str} - {start_hour}:{start_minute}{start_am_pm} to {end_hour}:{end_minute}{end_am_pm} ({event_type}:{task_name})'
-                    
+                    time_output = f'{month} {day}, {year} - {start_hour}:{start_minute}{start_am_pm} to {end_hour}:{end_minute}{end_am_pm} ({event_type}:{task_name})'
+                    time_str = f'{current_date_str} - {start_hour}:{start_minute}{start_am_pm} to {end_hour}:{end_minute}{end_am_pm}'
+
                     # Write the task to the file
                     with open('user_input.txt', 'a') as f:
                         f.write(f"{time_str}\n")
@@ -317,12 +326,7 @@ class TimePicker(tk.Frame):
                     # Increment the year by one
                     current_year += 1
                 
-                self.msg_display.config(text=f"Your appointment is booked for {time_str}:{freq}")
+                self.msg_display.config(text=f"Your appointment is booked for {time_output}:{freq}")
             
-
-            
-
-
-
-
-
+        elif event_type == 'Anti-Task':
+            print("Work in progress")
